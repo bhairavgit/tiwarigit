66735e600ef59f175f991cc1e742f401549568c2
bhairav <bhairavkali21@gmail.com>
2019-10-18 21:32:45 +0530

    added python.py

---
+++ python.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+print "git --version"
+print "sudo gitconfig --global user.email bhairavkali21@gmail.com"
+print "git config --global user.name "bhairav""
+print 
+print "give me a bottle of rum!"
